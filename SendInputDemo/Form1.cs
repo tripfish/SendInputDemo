00c5cd9b6daa9c08dbead442354858d58ee76e10
tripfish <4kbyte@gmail.com>
2023-03-05 15:30:06 +0100

    Add files via upload

---
+++ SendInputDemo/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace SendInputDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            InputSimulator inputSimulator = new InputSimulator();
+            inputSimulator.Keyboard.TextEntry("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+        }
+    }
+}
